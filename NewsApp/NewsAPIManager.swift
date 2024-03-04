e5a3a37513f48f30a3f7ba6d8565aa5bd1aac337
Negin Zahedi <zahedi.negin@gmail.com>
2024-03-04 17:45:33 -0500

    Add Models for API response

--- NewsApp/NewsAPIManager.swift
+++ NewsApp/NewsAPIManager.swift
@@ -10,6 +10,35 @@ import Foundation
 class NewsAPIManager {
     
     // MARK: - Properties
+    
     static let shared = NewsAPIManager()
     static let topHeadLinesURL = URL(string:"https://newsapi.org/v2/top-headlines?country=us&apiKey=\(Configuration.myAPIKey)")
+    
+    @Published var articles = [Article]()
+    
 }
+
+// MARK: - Models
+
+struct NewsAPIResponse: Codable{
+    let status: String?
+    let totalResults: Int?
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+    let content: String?
+}
+
+struct Source: Codable{
+    let id: String?
+    let name: String?
+}
+
