a316779b736c05f79b102d6cd713836c54dad7c4
Negin Zahedi <zahedi.negin@gmail.com>
2024-03-04 18:02:05 -0500

    Add UITableView

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -10,10 +10,23 @@ class ViewController: UIViewController {
     // MARK: - Variables
     private var articles: [Article] = []
     
+    // MARK: - UI Components
+    private var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.setupUI()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        fetchNews()
     }
     
     
@@ -22,7 +35,8 @@ class ViewController: UIViewController {
         self.title = "News App"
         self.view.backgroundColor = .systemBackground
         
-        fetchNews()
+        self.view.addSubview(tableView)
+        self.tableView.frame = view.bounds
     }
     
     // MARK: - Methods
@@ -37,3 +51,31 @@ class ViewController: UIViewController {
     }
 }
 
+// MARK: - Extention
+extension ViewController:  UITableViewDelegate, UITableViewDataSource {
+    
+    // number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    // what cell we want to use to pass info
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // custom NewsTableViewCell
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "cell",
+            for: indexPath
+        )
+        cell.backgroundColor = .secondaryLabel
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
+}
